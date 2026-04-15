4ed2168373dbe88e1d10a1c2559a29ca6ba4dc68
Guilherme Secone <guilhermeseconeneves@gmail.com>
2026-04-15 20:02:58 -0300

    instanciando o codigo

--- src/Application/Main.java
+++ src/Application/Main.java
@@ -1,36 +1,30 @@
-package Application;
 
-import Entities.Aluno;
+package Application;
+import util.Calculator;
 
 import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
 
-    public static final double PI = 3.14;
-
     public static void main(String[] args) {
 
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
+        Calculator calculator = new Calculator();
 
         System.out.print("Digite o raio: ");
         double raio = sc.nextDouble();
 
-        double c = circunferencia(raio);
-        double v = volume(raio);
+        double c = calculator.circunferencia(raio);
+        double v = calculator.volume(raio);
 
         System.out.println(c);
         System.out.println(v);
-        System.out.println(PI);
+        System.out.println(calculator.PI);
 
         sc.close();
     }
 
-    public static double circunferencia(double raio) {
-        return raio * 2 * PI;
-    }
-    public static double volume(double raio) {
-        return (4 * PI * Math.pow(raio, 3) ) / 3;
-    }
+
 }
