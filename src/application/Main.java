e319516d3232018dea2e9c7f2e4730a1b267c990
Guilherme Secone <guilhermeseconeneves@gmail.com>
2026-09-14 20:28:56 -0300

    so sera necessario chamar o nome da classe acompanhado do metodo, e nao mais instanciar um objeto, uma vez que os metodos sao estaticos e nao dependem de objetos

--- src/application/Main.java
+++ src/application/Main.java
@@ -7,15 +7,14 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Calculator calculator = new Calculator();
 
         System.out.print("Digite o raio: ");
         double raio = sc.nextDouble();
 
-        double c =  calculator.circunferencia(raio);
-        double v = calculator.volume(raio);
+        double c =  Calculator.circunferencia(raio);
+        double v = Calculator.volume(raio);
 
-        System.out.println("PI " + calculator.PI);
+        System.out.println("PI " + Calculator.PI);
         System.out.println("Circunferencia: " + c);
         System.out.println("Volume: " + v);
 
