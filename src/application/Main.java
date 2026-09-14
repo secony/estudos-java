dd3a9bec323377b193354b5534c3bb276d2874a8
Guilherme Secone <guilhermeseconeneves@gmail.com>
2026-09-14 20:19:23 -0300

    criando um classe para usar membros nao estaticos dentro de uma classe estatica instanciando o objeto

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,29 +1,25 @@
 package application;
+import util.Calculator;
+
 import java.util.Scanner;
 
 public class Main {
 
-    public static final double PI = 3.14;
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        Calculator calculator = new Calculator();
 
         System.out.print("Digite o raio: ");
         double raio = sc.nextDouble();
 
-        double c =  circunferencia(raio);
-        double v = volume(raio);
+        double c =  calculator.circunferencia(raio);
+        double v = calculator.volume(raio);
 
-        System.out.println("PI " + PI);
+        System.out.println("PI " + calculator.PI);
         System.out.println("Circunferencia: " + c);
         System.out.println("Volume: " + v);
 
         sc.close();
     }
-    public static double circunferencia(double raio){
-        return (2 * PI * raio);
-    }
-    public static double volume(double raio){
-        return (4* PI * raio *  raio * raio) /3;
-    }
+
 }
